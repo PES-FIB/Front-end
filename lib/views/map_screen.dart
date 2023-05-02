1e586fdf1c27dd281f1715ff862df74cc1d61eab
marta.campana <marta.campana@estudiantat.upc.edu>
2023-05-02 10:20:14 +0200

    google maps plugin settings

--- lib/views/map_screen.dart
+++ lib/views/map_screen.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class Map extends StatelessWidget {
+class Map extends StatefulWidget {
+  @override
+  _MapState createState() => _MapState();
+}
+
+class _MapState extends State<Map> {
+  static const initialCameraPosition = CameraPosition(target: LatLng(41.4040, 2.1744), zoom: 11.5);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.blue,
-        child: Center(
-          child: Text('Map'),
-        ),
-      ),
+      body: GoogleMap(
+        initialCameraPosition: initialCameraPosition),
     );
   }
 }
