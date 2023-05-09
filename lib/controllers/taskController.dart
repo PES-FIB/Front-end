3c42dc61c45135640389395677d37a4baa986115
gerardgv02 <gerard.garcia.velasco@estudiantat.upc.edu>
2023-05-09 11:46:48 +0200

    Keep on developing tasks controller

--- lib/controllers/taskController.dart
+++ lib/controllers/taskController.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-import 'dart:js_util';
 import 'package:flutter/material.dart';
 import 'package:downloads_path_provider_28/downloads_path_provider_28.dart';
 import 'package:http/http.dart' as http;
@@ -21,44 +20,112 @@ import 'package:permission_handler/permission_handler.dart';
 import 'dart:async';
 
 class taskController {
-  static void /*Future<Map<DateTime,List<Task>>>*/ getAllTasks() {
-    Map<DateTime, List<Task>> m = {};
-    return null;
+  // ignore: non_constant_identifier_names
+  static Future<Map<DateTime, List<Task>>> getAllTasks() async {
+    Map<DateTime, List<Task>> tasks = {};
+    Response r;
+    try {
+      r = await dio.get('${taskApis.getTaskUrl()}/${User.id}');
+    } catch (e) {
+      print(e);
+      return tasks;
+    }
+    if (r.statusCode == 200) {
+      if (r.data['data'] != null) {
+        for (int i = 0; i < r.data['data'].length; ++i) {
+          String name = r.data['data'][i]['name'];
+
+          String description;
+          if (r.data['data'][i]['description'] != null) {
+            description = r.data['data'][i]['description'];
+          } else {
+            description = '';
+          }
+
+          String initialDate = r.data['data'][i]['initial_date'];
+
+          String finalDate = r.data['data'][i]['final_date'];
+
+          int id = r.data['data'][i]['id'];
+
+          int code = r.data['data'][i]['code'];
+
+          String repeats;
+          if (r.data['data'][i]['repeats'] != null) {
+            repeats = r.data['data'][i]['repeats'];
+          } else {
+            repeats = '';
+          }
+        }
+      }
+    }
   }
 
   // ignore: non_constant_identifier_names
-  static Future<int> createTask(String name, String description, String initial_date, String final_date, String repeats) async {
+  static Future<int> createTask(String name, String description,
+      String initial_date, String final_date, String repeats) async {
     Response r;
     try {
       r = await dio.post(taskApis.getTaskUrl(), data: {
         "name": name,
         "description": description,
         "initial_d": initial_date.substring(0, initial_date.indexOf(' ')),
-        "initial_h": initial_date.substring(initial_date.lastIndexOf(' ')+ 1, initial_date.length),
-        "final_d":  final_date.substring(0, final_date.indexOf(' ')),
-        "final_h": final_date.substring(final_date.lastIndexOf(' ')+ 1, final_date.length),
+        "initial_h": initial_date.substring(
+            initial_date.lastIndexOf(' ') + 1, initial_date.length),
+        "final_d": final_date.substring(0, final_date.indexOf(' ')),
+        "final_h": final_date.substring(
+            final_date.lastIndexOf(' ') + 1, final_date.length),
         "repeats": repeats,
         "user_id": User.id
       });
     } catch (e) {
       return -1;
     }
-    return 1;
+    if (r.statusCode == 200) {
+      String description;
+      if (r.data['data']['description'] != null) {
+        description = r.data['data']['description'];
+      } else {
+        description = '';
+      }
+
+      String repeats;
+      if (r.data['data']['repeats'] != null) {
+        repeats = r.data['data']['repeats'];
+      } else {
+        repeats = '';
+      }
+
+      Task t = Task(
+          r.data['data']['id'],
+          r.data['data']['code'],
+          r.data['data']['name'],
+          description,
+          r.data['data']['initial_date'],
+          r.data['data']['final_date'],
+          repeats);
+      addTaskLocale(t);
+      return 1;
+    }
   }
 
   static void addTaskLocale(Task t) {
-    if (AppEvents.tasksCalendar.containsKey(DateUtils.dateOnly(DateTime.parse(t.initial_date)))) {
-    AppEvents.tasksCalendar[DateUtils.dateOnly(DateTime.parse(t.initial_date))]?.add(t);
-    }
-    else {
+    if (AppEvents.tasksCalendar
+        .containsKey(DateUtils.dateOnly(DateTime.parse(t.initial_date)))) {
+      AppEvents
+          .tasksCalendar[DateUtils.dateOnly(DateTime.parse(t.initial_date))]
+          ?.add(t);
+    } else {
       List<Task> l = [t];
-       AppEvents.tasksCalendar[DateUtils.dateOnly(DateTime.parse(t.initial_date))] = l;
+      AppEvents.tasksCalendar[
+          DateUtils.dateOnly(DateTime.parse(t.initial_date))] = l;
     }
   }
 
   static void deleteTask(Task t) {}
 
   static void deleteTaskLocale(Task t) {
-    AppEvents.tasksCalendar[DateUtils.dateOnly(DateTime.parse(t.initial_date))]?.remove(t);
+    AppEvents.tasksCalendar[DateUtils.dateOnly(DateTime.parse(t.initial_date))]
+        ?.remove(t);
   }
 }
--- lib/models/Task.dart
+++ lib/models/Task.dart
@@ -1,5 +1,7 @@
 class Task{
 
-  String code, name, description, initial_date, final_date, repeats;
-  Task(this.code, this.name, this.description, this.initial_date, this.final_date, this.repeats);
+  String name, description, initial_date, final_date, repeats;
+  int id;
+  int code;
+  Task(this.id, this.code, this.name, this.description, this.initial_date, this.final_date, this.repeats);
 }
